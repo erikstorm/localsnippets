905abbaff627d7cdd46e6be28547c82d93dedf6b
erikstorm <erik.storm@metropolia.fi>
2018-10-14 14:25:40 +0300

    Updates

---
+++ precache-manifest.d3525ac8d45aec88b194a5546f159842.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/localsnippets/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/localsnippets/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/localsnippets/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/localsnippets/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "6abcb076b8dbdf3ac5d1",
+    "url": "/localsnippets/static/js/runtime~main.6abcb076.js"
+  },
+  {
+    "revision": "a7cee7f26c2437f2175d",
+    "url": "/localsnippets/static/js/main.a7cee7f2.chunk.js"
+  },
+  {
+    "revision": "d34163f23c3c803911f6",
+    "url": "/localsnippets/static/js/1.d34163f2.chunk.js"
+  },
+  {
+    "revision": "a7cee7f26c2437f2175d",
+    "url": "/localsnippets/static/css/main.dc08a7b1.chunk.css"
+  },
+  {
+    "revision": "d34163f23c3c803911f6",
+    "url": "/localsnippets/static/css/1.b06c7dfc.chunk.css"
+  },
+  {
+    "revision": "5b8fedc65dbd2a88ef1ea287c36a7be1",
+    "url": "/localsnippets/index.html"
+  }
+];
